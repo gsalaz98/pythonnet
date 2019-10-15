2380fcacc21b579425f58b5e0bb38f44711a3927
Martin Molinero <martin.molinero1@gmail.com>
2019-10-15 16:29:39 -0300

    Implement PyList to C# list conversion

--- Python.Runtime/converter.cs
+++ Python.Runtime/converter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -179,17 +180,17 @@ class GMT(tzinfo):
 
             if (value is IList && !(value is INotifyPropertyChanged) && value.GetType().IsGenericType)
             {
-                using (var resultlist = new PyList())
+                using (var resultList = new PyList())
                 {
-                    foreach (object o in (IEnumerable)value)
+                    for (var i = 0; i < list.Count; i++)
                     {
-                        using (var p = new PyObject(ToPython(o, o?.GetType())))
+                        using (var p = list[i].ToPython())
                         {
-                            resultlist.Append(p);
+                            resultList.Append(p);
                         }
                     }
-                    Runtime.XIncref(resultlist.Handle);
-                    return resultlist.Handle;
+                    Runtime.XIncref(resultList.Handle);
+                    return resultList.Handle;
                 }
             }
 
@@ -383,6 +384,15 @@ class GMT(tzinfo):
                 Runtime.XIncref(value); // PyObject() assumes ownership
                 result = new PyObject(value);
                 return true;
+            }
+
+            if(obType.IsGenericType && Runtime.PyObject_TYPE(value) == Runtime.PyListType)
+            {
+                var typeDefinition = obType.GetGenericTypeDefinition();
+                if (typeDefinition == typeof(List<>))
+                {
+                    return ToList(value, obType, out result, setError);
+                }
             }
 
             // Common case: if the Python value is a wrapped managed object
@@ -1028,7 +1038,6 @@ class GMT(tzinfo):
             Exceptions.SetError(Exceptions.TypeError, $"Cannot convert {src} to {target}");
         }
 
-
         /// <summary>
         /// Convert a Python value to a correctly typed managed array instance.
         /// The Python value must support the Python iterator protocol or and the
@@ -1040,7 +1049,7 @@ class GMT(tzinfo):
             int size = Runtime.PySequence_Size(value);
             result = null;
 
-            if (size < 0 || elementType.IsGenericType)
+            if (elementType.IsGenericType)
             {
                 if (setError)
                 {
@@ -1075,10 +1084,8 @@ class GMT(tzinfo):
             items = Array.CreateInstance(elementType, list.Count);
             list.CopyTo(items, 0);
 
-            result = items;
             return true;
         }
-
 
         /// <summary>
         /// Convert a Python value to a correctly typed managed enum instance.
